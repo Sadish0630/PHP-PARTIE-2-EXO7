b6aa6fd518b659babafe369b5b1d186f23fada3d
audemar <audemarsadish@gmail.com>
2021-12-08 11:12:15 +0100

    exo7 terminé

---
+++ exo7.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css" />
+    <title>PARTIE 2 EXO 7</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $isOk = 1;
+            if ($isOk == 0) {
+                echo'c\'est pas bon !!!';}
+            else {
+                echo 'c\'est ok !!';}
+        ?>
+    </p>
+
+</body>
+
+</html>
